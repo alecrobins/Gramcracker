4e014eae04592683a47108476b5020a75b9b9ff2
Alec Robins <alecrobins@gmail.com>
2015-08-08 22:25:16 -0400

    no location defalts to thults to the current location

--- build/app.js
+++ build/app.js
@@ -26290,23 +26290,41 @@ var SearchBar = React.createClass({displayName: "SearchBar",
 		};
 
 		// if search is empty then get current location
-		// if(searchData.location === "" && navigator.geolocation){
-  //  	   navigator.geolocation.getCurrentPosition(
-  //  	   	function(position){
-  //  	   		var lat = position.coords.latitude;
-  //  	   		var lng = position.coords.longitude;
-  //  	   		var geocoder = new google.maps.Geocoder();
-  //  	   		var latlng = new google.maps.LatLng(lat, lng);
+		if(searchData.location === "" && navigator.geolocation){
+   	   navigator.geolocation.getCurrentPosition(
+   	   	function(position){
+   	   		var lat = position.coords.latitude;
+   	   		var lng = position.coords.longitude;
+   	   		var geocoder = new google.maps.Geocoder();
+   	   		var latlng = new google.maps.LatLng(lat, lng);
     
-  //   				geocoder.geocode({'latLng': latlng}, function(results, status) {
-  //     				if (status == google.maps.GeocoderStatus.OK) {
-  //     					console.log(results[0].formatted_address);
-  //     				}
-  //     			});
-  //  	   	});
-		// }else{
-		// 	completeSearch();
-		// }
+    				geocoder.geocode({'latLng': latlng}, function(results, status) {
+      				if (status == google.maps.GeocoderStatus.OK) {
+      					var city, state;
+      					
+      					for(var i in results[0].address_components){
+      						var component = results[0].address_components[i];
+      						
+      						if(component.types[0] === "administrative_area_level_1"){
+      							state = component.long_name;
+      						}
+
+      						if(component.types[0] === "locality"){
+      							city = component.long_name;
+      						}
+
+      					}
+
+      					// reset the location information before sending off search
+      					searchData.location = city + ", " + state;
+
+      					completeSearch(); 
+      				}
+      			});
+   	   	});
+		}else{
+      	completeSearch();		
+		}
 
 		var completeSearch = function(){
 			// delete the null
@@ -26318,9 +26336,10 @@ var SearchBar = React.createClass({displayName: "SearchBar",
 
 			// send off the search with searchData as the query
      		self.context.router.transitionTo('search', null, searchData);
+
 		};
 
-		completeSearch();
+		// completeSearch();
 
 	},
 
@@ -26329,16 +26348,15 @@ var SearchBar = React.createClass({displayName: "SearchBar",
 	render: function() {
 		return (
 			React.createElement("div", {className: "home--search-container"}, 
-				React.createElement("form", {onSubmit: this.handleSubmit}, 
-					React.createElement("label", {htmlFor: "term"}, "Term"), 
-					React.createElement("input", {type: "text", name: "term", ref: "term"}), 
+			
+				React.createElement("label", {htmlFor: "term"}, "Term"), 
+				React.createElement("input", {type: "text", name: "term", ref: "term"}), 
 					
-					React.createElement("label", {htmlFor: "location"}, "Location"), 
-					React.createElement("input", {type: "text", name: "location", ref: "location"}), 	
+				React.createElement("label", {htmlFor: "location"}, "Location"), 
+				React.createElement("input", {type: "text", name: "location", ref: "location"}), 	
 					
-					React.createElement("input", {type: "submit", className: "submitForm"})
+				React.createElement("i", {className: "fa  fa-search fa-3x", onClick: this.handleSubmit})
 
-				)
 			)
 		);
 	}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -19,23 +19,41 @@ var SearchBar = React.createClass({
 		};
 
 		// if search is empty then get current location
-		// if(searchData.location === "" && navigator.geolocation){
-  //  	   navigator.geolocation.getCurrentPosition(
-  //  	   	function(position){
-  //  	   		var lat = position.coords.latitude;
-  //  	   		var lng = position.coords.longitude;
-  //  	   		var geocoder = new google.maps.Geocoder();
-  //  	   		var latlng = new google.maps.LatLng(lat, lng);
+		if(searchData.location === "" && navigator.geolocation){
+   	   navigator.geolocation.getCurrentPosition(
+   	   	function(position){
+   	   		var lat = position.coords.latitude;
+   	   		var lng = position.coords.longitude;
+   	   		var geocoder = new google.maps.Geocoder();
+   	   		var latlng = new google.maps.LatLng(lat, lng);
     
-  //   				geocoder.geocode({'latLng': latlng}, function(results, status) {
-  //     				if (status == google.maps.GeocoderStatus.OK) {
-  //     					console.log(results[0].formatted_address);
-  //     				}
-  //     			});
-  //  	   	});
-		// }else{
-		// 	completeSearch();
-		// }
+    				geocoder.geocode({'latLng': latlng}, function(results, status) {
+      				if (status == google.maps.GeocoderStatus.OK) {
+      					var city, state;
+      					
+      					for(var i in results[0].address_components){
+      						var component = results[0].address_components[i];
+      						
+      						if(component.types[0] === "administrative_area_level_1"){
+      							state = component.long_name;
+      						}
+
+      						if(component.types[0] === "locality"){
+      							city = component.long_name;
+      						}
+
+      					}
+
+      					// reset the location information before sending off search
+      					searchData.location = city + ", " + state;
+
+      					completeSearch(); 
+      				}
+      			});
+   	   	});
+		}else{
+      	completeSearch();		
+		}
 
 		var completeSearch = function(){
 			// delete the null
@@ -47,9 +65,10 @@ var SearchBar = React.createClass({
 
 			// send off the search with searchData as the query
      		self.context.router.transitionTo('search', null, searchData);
+
 		};
 
-		completeSearch();
+		// completeSearch();
 
 	},
 
@@ -58,16 +77,15 @@ var SearchBar = React.createClass({
 	render: function() {
 		return (
 			<div className = "home--search-container">
-				<form onSubmit={this.handleSubmit}>
-					<label htmlFor="term">Term</label>
-					<input type="text" name="term" ref="term" />
+			
+				<label htmlFor="term">Term</label>
+				<input type="text" name="term" ref="term" />
 					
-					<label htmlFor="location">Location</label>
-					<input type="text" name="location" ref="location" />	
+				<label htmlFor="location">Location</label>
+				<input type="text" name="location" ref="location" />	
 					
-					<input type="submit" className="submitForm" />
+				<i className="fa  fa-search fa-3x" onClick={this.handleSubmit}></i>
 
-				</form>
 			</div>
 		);
 	}
